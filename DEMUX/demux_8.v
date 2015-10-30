dd0f0408225bef864e5c37430b7e20d308a71e8a
meetshah1995 <meetshah1995@iitb.ac.in>
2015-10-31 04:42:25 +0530

    Add Memory and Hazard

---
+++ DEMUX/demux_8.v
@@ -0,0 +1,19 @@
+module demux_8(selectInput, out);  // 8-output decoder
+
+	output reg [7:0] out;
+	input  [2:0] selectInput;
+	
+	always@(selectInput) begin
+		case(selectInput)
+			0: out = 8'b00000001;
+			1: out = 8'b00000010;
+			2: out = 8'b00000100;
+			3: out = 8'b00001000;
+			4: out = 8'b00010000;
+			5: out = 8'b00100000;
+			6: out = 8'b01000000;
+			7: out = 8'b10000000;
+		endcase
+	end
+	
+endmodule
