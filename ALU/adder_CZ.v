e447d25a99deb2d9c35e7308e141f36f12c39137
meetshah1995 <meetshah1995@iitb.ac.in>
2015-10-31 04:20:53 +0530

    Add Equality Check and NAND

---
+++ ALU/adder_CZ.v
@@ -0,0 +1,12 @@
+module adder_CZ(in1, in2 , out, carry);		// Implements a full 16-bit adder
+	
+	output [15:0] out;
+	output        carry;
+	input  [15:0] in1, in2;
+	wire   [16:0] outTemp;
+
+	assign outTemp = in1 + in2;
+	assign out     = outTemp[15:0];
+	assign carry   = outTemp[16];
+	
+endmodule
